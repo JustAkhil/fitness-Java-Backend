19262aef9971ce31b058ad63b67fcdd92533bf5d
Akhil <sahuv3694@gmail.com>
2026-07-09 01:52:55 +0530

    Initial commit

---
+++ src/main/java/com/project/fitness/model/Recommendation.java
@@ -0,0 +1,61 @@
+package com.project.fitness.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Recommendation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String type;
+
+
+    // in sql then char limit is 250 we are modifying the limit
+    // upto 2k using column annotation
+    @Column(length = 2000)
+    private String recommendation;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    private List<String> improvements;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    private List<String> suggestions;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    private List<String> safety;
+    private LocalDateTime createdAt;
+    private  LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "activity_id",nullable = false,foreignKey = @ForeignKey(name = "fk_recommendation_activity"))
+    @JsonIgnore
+    private Activity activity;
+
+
+
+    // lazy mean if user fetch it not fetch recommendation
+    // until user fetch it
+    //eager means when user fetch recommendation will fetch automatically
+    @ManyToOne(fetch =  FetchType.LAZY)
+    @JoinColumn(name = "user_id",nullable = false,foreignKey = @ForeignKey(name = "fk_recommendation_user"))
+    @JsonIgnore
+    private User user;
+}
---
+++ src/main/java/com/project/fitness/model/User.java
@@ -0,0 +1,47 @@
+package com.project.fitness.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+// data is  the combination of setter and getter of the field used in class
+// this lombok automatically generate getter and setter and reduce boilerplate code
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+    // orphan removal means if user deleted then all the thing deleted regarding that deleted user
+    // is useless so we use orphan true
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
+    @JsonIgnore
+    private List<Activity>activities=new ArrayList<>();
+
+//    If Entity A has Entity B
+//    and Entity B also has Entity A
+//    then JSON loop can happen.
+//    so that we use json ignore
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
+    @JsonIgnore
+    private List<Recommendation>recommendations=new ArrayList<>();
+}
